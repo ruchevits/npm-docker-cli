5d8c40017944ff2562e0ea1235e52f617b34ec05
Edward Ruchevits <ruchevits@gmail.com>
2017-06-09 19:40:34 +0100

    Building containers, running them locally and deploying to EC2 container service

--- bin/npm-docker-cli.sh
+++ bin/npm-docker-cli.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
+NPM_REGISTRY_TOKEN="JSXGUN/1pN5nNr/EDzWSBK3h4h3jOB0Un5GT4I84bRU="
+
 get_abs_filename() {
   echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
 get_docker_image_name() {
   PACKAGE_NAME=$(grep '"name":' $1 | cut -d\" -f4 | cut -c 2-)
+  echo "$PACKAGE_NAME"
+}
+
+get_docker_image_version() {
   PACKAGE_VERSION=$(grep '"version":' $1 | cut -d\" -f4)
-  echo "$PACKAGE_NAME:$PACKAGE_VERSION"
+  echo "$PACKAGE_VERSION"
+}
+
+get_aws_repository_url() {
+  REPOSITORY_URL=$(grep '"repository":' $1 | cut -d\" -f4)
+  echo "$REPOSITORY_URL"
 }
 
 PROGRAM_NAME=$(basename $0)
@@ -22,14 +33,23 @@ then
   exit 1
 fi
 
-# Read package name version
+# Read package name and version
 DOCKER_IMAGE_NAME=$(get_docker_image_name $PACKAGE_FILE)
+DOCKER_IMAGE_VERSION=$(get_docker_image_version $PACKAGE_FILE)
+
+# Truncate 'tallysticks/' from the docker image name
+DOCKER_CONTAINER_NAME=$(echo $DOCKER_IMAGE_NAME | cut -c 13-)
 
 sub_help(){
     echo "Usage: $PROGRAM_NAME <subcommand> [options]"
     echo "Subcommands:"
+    echo "    help       Display this information"
     echo "    build      Build docker image based on name and version from the package.json file"
+    # echo "    create     Create new docker container based on the built image"
     echo "    deploy     Deploy docker image to a specified repository on EC2 container service"
+    echo "    run        Run docker container"
+    # echo "    stop       Stop running container"
+    # echo "    restart    Restart running container"
 }
 
 sub_build() {
@@ -44,14 +64,20 @@ sub_build() {
     exit 1
   fi
 
-  echo "Building docker image: $DOCKER_IMAGE_NAME"
+  echo "Building docker image: $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION"
 
-  # TODO: Build docker image
-  # docker build -t $DOCKER_IMAGE_NAME $DOCKER_FILE
-  echo "Error: not implemented"
-  exit 1
+  # Build docker image
+  docker build \
+    --no-cache \
+    --build-arg NPM_REGISTRY_TOKEN=$NPM_REGISTRY_TOKEN \
+    -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION \
+    -f $DOCKER_FILE .
 
 }
+
+# sub_create() {
+#   # docker create --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
+# }
 
 sub_deploy() {
 
@@ -65,13 +91,31 @@ sub_deploy() {
     exit 1
   fi
 
-  echo "Deploying docker image: $DOCKER_IMAGE_NAME"
+  AWS_REPOSITORY_URL=$(get_aws_repository_url $DEPLOYMENT_FILE)
 
-  # TODO: deploy container
-  echo "Error: not implemented"
-  exit 1
+  echo "Deploying docker image: $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION to $AWS_REPOSITORY_URL"
+
+  docker tag $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION $AWS_REPOSITORY_URL:$DOCKER_IMAGE_VERSION
+  docker push $AWS_REPOSITORY_URL:$DOCKER_IMAGE_VERSION
 
 }
+
+sub_run() {
+
+  docker run -d --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
+
+  # TODO: save returned container ID (e.g. 59cfbc659bf7f17488c9a6c327e85a4945376fb4405a3037d0c9b0dbb69ea045)
+  # TODO: 
+
+}
+
+# sub_stop() {
+#   # TODO: stop container using the container ID saved in sub_run()
+# }
+
+# sub_restart() {
+#   # TODO: restart container using the container ID saved in sub_run()
+# }
 
 SUB_COMMAND=$1
 
